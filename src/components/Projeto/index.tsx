9f5862dd0eb8d06a52935c74be294c5899270bfc
Fernando Pinho <fernandoalbertopinho20@gmail.com>
2023-06-17 18:07:07 +0100

    terminar

--- src/components/Projeto/index.tsx
+++ src/components/Projeto/index.tsx
@@ -1,13 +1,14 @@
 import Paragrafo from '../Paragrafo'
 import Titulo from '../Titulo'
 
-import { Card } from './styles'
+import { Card, LinkBotao } from './styles'
 
 const Projeto = () => {
   return (
     <Card>
       <Titulo>Projeto Lista de tarefas</Titulo>
       <Paragrafo tipo="secundario">Lista de tarefas feita com VueJS</Paragrafo>
+      <LinkBotao>Visualizar</LinkBotao>
     </Card>
   )
 }
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/fernandopinho17.png" />
-    <Titulo fontSize={20}>Fernando Pinho</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Fernando Pinho</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        fernandopinho17
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
